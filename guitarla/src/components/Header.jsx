6abc48bfe11ca569a589d92dd31caadf6cf5baf9
Eliam Solis de Los Santos <eliamsolis2201@gmail.com>
2025-07-27 14:29:48 -0400

    create hooks

--- guitarla/src/components/Header.jsx
+++ guitarla/src/components/Header.jsx
@@ -1,8 +1,6 @@
 
-import { useMemo } from "react";
-export default function Header({ cart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart }) {
-  const isCartEmpty = useMemo(() => cart.length === 0, [cart])
-  const cartTotal = useMemo(() => cart.reduce((total, item) => total + (item.quantity * item.price), 0), [cart])
+export default function Header({ cart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart, isCartEmpty, cartTotal }) {
+
   return (
     <header className="py-5 header">
       <div className="container-xl">
