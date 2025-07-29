641a8ebc2c00c687a0c92be2538a01b158570a5e
Eliam Solis <eliamsolis2201@gmail.com>
2025-07-29 16:02:43 -0400

    nextjs proyect

---
+++ colmadon/app/products/page.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function page (props) {
+    console.log('Desde el server');
+  return (
+    <div>
+      PRODUCTS
+    </div>
+  )
+};
+
---
+++ colmadon/components/order/OrderSidebar.jsx
@@ -0,0 +1,9 @@
+
+export default function OrderSidebar (props) {
+  return (
+    <aside className="md:w-75 md:h-screen bg-white">
+      OrderSidebar
+    </aside>
+  )
+};
+
