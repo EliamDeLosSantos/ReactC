f5062001321c0ad4b0dcac51be5ea80bcb1fb433
Eliam Solis de Los Santos <eliamsolis2201@gmail.com>
2025-08-04 00:43:21 -0400

    finished calories tracker project

---
+++ calories-tracker/src/reducers/activityReducer.ts
@@ -0,0 +1,65 @@
+import type { Activity } from "../types"
+
+
+export type ActivityActions = 
+{ type: 'save-activity', payload: {newActivity:Activity} } |
+{ type: 'set-activeId', payload: {id:Activity['id']} } |
+{ type: 'delete-activity', payload: {id:Activity['id']} } |
+{ type: 'restart-app'} 
+
+export type ActivityState = {
+    activities: Activity[],
+    activeId: Activity['id']
+}
+
+const localStorageActivities = (): Activity[] =>{
+    const activities = localStorage.getItem('activities')
+    return activities ? JSON.parse(activities): []
+}
+
+export const initialState: ActivityState = {
+    activities: localStorageActivities(),
+    activeId: ''
+}
+
+export const activityReducer = (
+    state: ActivityState = initialState,
+    action:ActivityActions
+) => {
+    if(action.type === 'save-activity'){
+        let upsertedActivity: Activity[] = []
+        if(state.activeId){
+            upsertedActivity = state.activities
+                .map(stateActivity => stateActivity.id === state.activeId ? action.payload.newActivity : stateActivity)
+        }else {
+            upsertedActivity = [...state.activities, action.payload.newActivity]
+        }
+        return {
+            ...state,
+            activities: upsertedActivity,
+            activeId: ''
+        }
+    }
+    if(action.type === 'set-activeId'){
+        return {
+            ...state,
+            activeId: action.payload.id
+        }
+    }
+    if(action.type === 'delete-activity'){
+        console.log(action.payload.id)
+        const filteredActivities = state.activities.filter(stateActivity => stateActivity.id !== action.payload.id)
+        return {
+            ...state,
+            activities: filteredActivities
+        }
+    }
+    if(action.type === 'restart-app'){
+        return {
+            activities: [],
+            activeId: ''
+        }
+    }
+    return state
+}
+
