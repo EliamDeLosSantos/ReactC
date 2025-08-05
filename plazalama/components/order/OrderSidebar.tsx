278a5c1d5fbecab842792ca297a6a80036856b19
Eliam Solis <eliamsolis2201@gmail.com>
2025-08-05 16:20:34 -0400

    add plazalama nextjs project

---
+++ plazalama/components/order/OrderSidebar.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function OrderSidebar () {
+  return (
+    <section className="md:w-72 md:h-screen bg-white">
+        ORDERSIDEBAR
+    </section>
+  )
+};
+
