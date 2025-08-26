b3dd88bf30499d7acb1d19cd82ce6bd2a3415129
Eliam Solis <eliamsolis2201@gmail.com>
2025-08-26 16:09:48 -0400

    finished pagination

--- plazalama/app/admin/products/page.tsx
+++ plazalama/app/admin/products/page.tsx
@@ -2,8 +2,15 @@ import Heading from "@/components/ui/Heading"
 import React from "react"
 import { prisma } from "@/src/lib/prisma"
 import ProductTable from "@/components/products/ProductsTable";
+import ProductsPagination from "@/components/products/ProductsPagination";
+import { redirect } from "next/navigation";
+import Link from "next/link";
 
-export async function getProducts(pageSize: number = 10, page: number = 1){
+export async function productsCount() {
+  return prisma.product.count()
+}
+
+export async function getProducts(pageSize: number = 10, page: number = 1) {
   const skip = (page - 1) * pageSize
   const products = prisma.product.findMany({
     take: pageSize,
@@ -17,16 +24,35 @@ export async function getProducts(pageSize: number = 10, page: number = 1){
 //Folma dura de inferir types en TS
 export type ProductsWithCategory = Awaited<ReturnType<typeof getProducts>>
 
-export default async function ProductsPage ({searchParams} : {searchParams: { page:string }}) {
-  const page = +searchParams.page || 1
+export default async function ProductsPage({ searchParams }: { searchParams: { page: string } }) {
+  const pageString = await searchParams
+  const page = +pageString.page || 1
+
+  if(page < 0 ) redirect("/admin/products")
+
   const pageSize = 10
-  const products = await getProducts(pageSize, page)
+  const productsData = await getProducts(pageSize, +page)
+  const totalProductsData = await productsCount()
+  const [products, totalProducts] = await Promise.all([productsData, totalProductsData])
+  const totalPages = Math.ceil(totalProducts / pageSize)
+
+  if (page > totalPages) redirect("/admin/products")
+
   return (
     <div>
       <Heading>
         Administrar Productos
       </Heading>
+      <div>
+        <Link
+          href={'/admin/products/new'}
+          className={'bg-amber-400 w-full lg:w-auto text-xl px-10 py-3 text-center font-bold cursor-pointer'}
+        >
+          Crear Productos
+        </Link>
+      </div>
       <ProductTable products={products} />
+      <ProductsPagination page={page} totalPages={totalPages} />
     </div>
   )
 };
---
+++ plazalama/components/products/ProductsSearchForm.tsx
@@ -0,0 +1,8 @@
+export default function ProductsSearchForm () {
+  return (
+    <form className="flex items-center">
+      <input className="p-2 placeholder-gray-400 w-full" />
+    </form>
+  )
+};
+
