5ee11d08bc443d4225a8833c48b120e8c73a5692
Eliam Solis <eliamsolis2201@gmail.com>
2026-04-29 17:18:19 -0400

    feat: Store methods, Modal and components added

--- bebidas-react/src/types/index.ts
+++ bebidas-react/src/types/index.ts
@@ -1,7 +1,8 @@
 import z from "zod";
-import type { CategoryAPIResponseSchema, DrinkAPIResponseSchema, DrinksAPIResponseSchema, SearchFilterSchema } from "../schemas";
+import type { CategoryAPIResponseSchema, DrinkAPIResponseSchema, DrinksAPIResponseSchema, RecipeAPIResponseSchema, SearchFilterSchema } from "../schemas";
 
 export type Categories = z.infer<typeof CategoryAPIResponseSchema>
 export type SearchFilter = z.infer<typeof SearchFilterSchema>
 export type DrinksAPIResponse = z.infer<typeof DrinksAPIResponseSchema>
 export type DrinkAPIResponse = z.infer<typeof DrinkAPIResponseSchema>
+export type RecipeAPIResponse = z.infer<typeof RecipeAPIResponseSchema>
